f4bd4b5258946e8823bb1580e0045ed49cc7c5d4
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-07-29 12:46:34 -0400

    Systemetize regridding of files in A07

--- bnu/A07_regrid.F90
+++ bnu/A07_regrid.F90
@@ -10,18 +10,53 @@ module a07_mod
 
 implicit none
 
+
 type IOFname_t
-    type(FileInfo_t) :: iinfo, oinfo
+    character*(512) :: root
+    character*(100) :: idir,odir
+    character*(100) :: ileaf,oleaf
     logical :: lc_weighting
+    character*(40) :: vname
+    character*(100) :: long_name
+    character*(30) :: units
+    character*(60) :: title
 end type IOFname_t
 
 CONTAINS
 
+function make_fname( &
+    root, idir, ileaf, odir, oleaf, lc_weighting, &
+    vname, long_name, units) result(fn)
+    character*(*), intent(IN) :: root, idir, ileaf, odir, oleaf
+    logical, intent(IN) :: lc_weighting
+    character*(*), intent(IN) :: vname, long_name, units
+    type(IOFname_t) :: fn
 
-function make_fname(ichunker, ochunker, ents, lc_weighting, &
+    fn%root = root
+    fn%idir = idir
+    fn%ileaf = ileaf
+    fn%odir = odir
+    fn%oleaf = oleaf
+    fn%lc_weighting = lc_weighting
+    fn%vname = vname
+    fn%long_name = long_name
+    fn%units = units
+
+end function make_fname
+
+
+!type IOFname_t
+!    type(FileInfo_t) :: iinfo, oinfo
+!    logical :: lc_weighting
+!end type IOFname_t
+!
+!CONTAINS
+
+
+function make_fname2(ichunker, ochunker, ents, lc_weighting, &
     laisource, cropsource, var, year, istep, ostep, ver, &
-    doytype, idoy, varsuffix
-) result(fn)
+    doytype, idoy, varsuffix) &
+result(fn)
     class(Chunker_t), intent(IN) :: ichunker, ochunker
     type(EntSet_t), intent(IN) :: ents
     logical, intent(IN) :: lc_weighting
@@ -37,21 +72,28 @@ function make_fname(ichunker, ochunker, ents, lc_weighting, &
     character*(*), intent(IN), OPTIONAL :: doytype ! ann,doy,month
     integer, intent(IN), OPTIONAL :: idoy
     character*(*), intent(IN), OPTIONAL :: varsuffix
+    type(IOFname_t) :: fn       ! RESULT
 
     ! ------------- Locals
-    type(IOFname_t) :: fn
+    type(FileInfo_t) :: iinfo, oinfo
 
-    fn%lc_weighting = lc_weighting
-    fn%iinfo = ichunker%file_info(ents, laisource, cropsource, var, year, istep, ver, doytype, idoy, varsuffix)
-    fn%oinfo = ochunker%file_info(ents, laisource, cropsource, var, year, ostep, ver, doytype, idoy, varsuffix)
-end function make_fname
+    call ichunker%file_info(iinfo, ents, &
+        laisource, cropsource, var, year, istep, ver, doytype, idoy, varsuffix)
+    call ochunker%file_info(oinfo, ents, &
+        laisource, cropsource, var, year, ostep, ver, doytype, idoy, varsuffix)
+
+    fn = make_fname(LC_LAI_ENT_DIR, &
+        iinfo%dir, iinfo%leaf, &
+        oinfo%dir, oinfo%leaf, &
+        lc_weighting, &
+        iinfo%vname, iinfo%long_name, iinfo%units)
+end function make_fname2
 
 
 subroutine regrid_lais(esub, fname)
-    type(GcmEntSet_t), intent(IN), taret :: esub
+    type(EntSet_t), intent(IN) :: esub
     type(IOFname_t),intent(IN), target :: fname(:)
     ! ------------ Local Vars
-    class(EntSet_t), pointer :: esub_p
     type(Chunker_t) :: chunker, chunkerlr
     type(ChunkIO_t) :: ioall_lc2, io_lc_pure(esub%ncover)
     type(ChunkIO_t) :: ioall_lai(size(fname,1)), io_lai(esub%ncover,size(fname,1))
@@ -68,32 +110,41 @@ subroutine regrid_lais(esub, fname)
     integer :: ndoy,idoy
     type(IOFname_t), pointer :: fn
     type(Weighting_t) :: wgt
+    logical :: need_lc
 
     ndoy = size(fname,1)
     do idoy=1,ndoy
-        print *,'****************** BEGIN Regrid ',trim(fname(idoy)%leaf)
+        print *,'****************** BEGIN Regrid ',trim(fname(idoy)%ileaf),' --> ',trim(fname(idoy)%oleaf)
     end do
 
-    call chunker%init(IM1km, JM1km, IMH*2,JMH*2, 'qxq', 300, 320)
-    call chunkerlr%init(IMLR,JMLR, IMH*2,JMH*2, 'qxq', 300, 320)
+    call chunker%init(IM1km, JM1km, IMH*2,JMH*2, 'qxq', 300, 320, 20)
+    call chunkerlr%init(IMLR,JMLR, IMH*2,JMH*2, 'qxq', 300, 320, 20)
 
     ! Hntr stuff
     spec_hr = hntr_spec(chunker%chunk_size(1), chunker%ngrid(2), 0d0, 180d0*60d0 / chunker%ngrid(2))
     spec_lr = hntr_spec(chunkerlr%chunk_size(1), chunkerlr%ngrid(2), 0d0, 180d0*60d0 / chunkerlr%ngrid(2))
     hntr_lr = hntr_calc(spec_lr, spec_hr, 0d0)   ! datmis=0
 
+    ! Figure out if we need LC weighting
+    need_lc = .false.
+    do idoy=1,ndoy
+        need_lc = need_lc.or.fname(idoy)%lc_weighting
+    end do
+
 
     ! ------------- Input Files
     ! LC written by A04; in the esub indexing scheme
-    call chunker%nc_open_set(esub_p, io_lc_pure, &
-        'BNU', 'M', 'lc', 2004, 'pure', '1.1')
+    if (need_lc) then
+        call chunker%nc_open_set(esub, io_lc_pure, &
+            'BNU', 'M', 'lc', 2004, 'pure', '1.1')
+    end if
 
     ! LAI
     do idoy=1,ndoy
         fn => fname(idoy)
 
         call chunker%nc_open(ioall_lai(idoy), trim(fn%root), &
-            trim(fn%idir), trim(fn%leaf)//'.nc', trim(fn%vname), 0)
+            trim(fn%idir), trim(fn%ileaf)//'.nc', trim(fn%vname), 0)
         do k = 1,esub%ncover
             call chunker%nc_reuse_var(ioall_lai(idoy), io_lai(k,idoy), (/1,1,k/))
         enddo
@@ -108,13 +159,13 @@ subroutine regrid_lais(esub, fname)
         end do
 
         call chunkerlr%nc_create(ioall_laiout(idoy), wgt, &
-            trim(fn%odir), trim(fn%leaf), trim(fn%vname), &
-            trim(fn%long_name), trim(fn%units), trim(fn%title), &
-            esub%mvs, esub%layer_names(), &
+            trim(fn%odir), trim(fn%oleaf), trim(fn%vname), &
+            trim(fn%long_name), trim(fn%units), &
+            esub%layer_names(), &
             create_lr=.false.)
         do k=1,esub%ncover
             call chunkerlr%nc_reuse_var(ioall_laiout(idoy), io_laiout(k,idoy), &
-                (/1,1,k/), weighting(io_lc_pure(k)%buf, 1d0,0d0))
+                (/1,1,k/), wgt)
         end do   ! k
 
     end do    ! idoy
@@ -122,6 +173,11 @@ subroutine regrid_lais(esub, fname)
     call chunker%nc_check('A07_regrid_hr')
     call chunkerlr%nc_check('A07_regrid_lr')
 
+!    call regrid_handles(chunker, chunkerlr, esub
+!
+!end subroutine regrid_lais
+!
+!subroutine regrid_handles()
 
 #ifdef ENTGVSD_DEBUG
     do jchunk = chunker%nchunk(2)*3/4,chunker%nchunk(2)*3/4+1
@@ -166,59 +222,54 @@ subroutine do_regrid_all_lais
     use gcm_labels_mod
     use chunkparams_mod
 
+    type(EntSet_t) :: ent1
     type(GcmEntSet_t), target :: esub
+    class(EntSet_t), pointer :: esub_p
+    type(FileInfo_t) :: iinfo,oinfo
 
     integer, parameter :: MAXFILES = 20 !(1 + (1 + 2 + 12))   ! LC + LAI(annual + doy + monthly)
-    type(IOFname_t) :: fname(MAXFILES)
+    type(IOFname_t) :: fname(MAXFILES),fnames1(1)
     integer :: nf,i0,i1,idoy,imonth
+    type(Chunker_t) :: chunker, chunkerlr
 
     call init_ent_labels
     esub = make_ent_gcm_subset(combine_crops_c3_c4, split_bare_soil)
+    esub_p => esub
+
+    ! Chunkers just for make_fname2()
+    call chunker%init(IM1km, JM1km, IMH*2,JMH*2, 'qxq', 0,0,0)
+    call chunkerlr%init(IMLR,JMLR, IMH*2,JMH*2, 'qxq', 0,0,0)
 
     nf = 0
 
     ! ----------- Annual
     nf = nf + 1
-    fname(nf) = make_fname(chunker, chunkerlr, esub, &
+    fname(nf) = make_fname2(chunker, chunkerlr, esub_p, &
         .false., 'BNU', 'M', 'lc', 2004, 'pure', 'purelr', '1.1')
 
 
     nf = nf + 1
-    fname(nf) = make_fname(chunker, chunkerlr, esub, &
-        .false., 'BNU', 'M', 'laimax', 2004, 'pure', 'purelr', '1.1')
-$$$$$$$$$$$$ WORKING HERE.... $$$$$$$$$
-    fname(nf) = make_fname(LC_LAI_ENT_DIR, &
-        'pure2/annual/', 'purelr/annual/', 'entmm29_ann_laimax', .true., &
-        'lai', 'Ent LAI (annual, DOY or monthly)', 'm^2 m-2', 'Leaf Area Index')
+    fname(nf) = make_fname2(chunker, chunkerlr, esub_p, &
+        .true., 'BNU', 'M', 'laimax', 2004, 'pure', 'purelr', '1.1')
 
     nf = nf + 1
-    fname(nf) = make_fname(LC_LAI_ENT_DIR, &
-        'pure2/annual/', 'purelr/annual/', 'entmm29_ann_height', .true., &
-        'SimardHeights', 'Plant Heights', 'm', 'Plant Heights')
-
-
-    ! Bare soil Bright Ratio
-    nf = nf + 1
-!print *,LAI3G_INPUT
-!stop
-    fname(nf) = make_fname(LAI3G_INPUT, &
-        'lc_lai_ent/', 'purelr/annual/', 'bs_brightratio', .false., &
-        'bs_brightratio', 'Bare Soil Brightness Ratio', '1', 'ratio')
+    fname(nf) = make_fname2(chunker, chunkerlr, esub_p, &
+        .true., 'BNU', 'M', 'hgt', 2004, 'pure', 'purelr', '1.1')
 
     ! ---------- DOY
     do idoy=1,NDOY
         nf = nf + 1
-        fname(nf) = make_fname(LC_LAI_ENT_DIR, &
-            'pure2/doy/', 'purelr/doy/', 'entmm29_'//DOY(idoy)//'_lai', .true., &
-            'lai', 'Ent LAI (annual, DOY or monthly)', 'm^2 m-2', 'Leaf Area Index')
+        fname(nf) = make_fname2(chunker, chunkerlr, esub_p, &
+            .true., 'BNU', 'M', 'lai', 2004, 'pure', 'purelr', '1.1', &
+            'doy', idoy)
     end do
 
     ! ---------- MONTH
     do imonth=1,NMONTH
         nf = nf + 1
-        fname(nf) = make_fname(LC_LAI_ENT_DIR, &
-            'pure2/monthly/', 'purelr/monthly/', 'entmm29_'//MONTH(imonth)//'_lai', .true., &
-            'lai', 'Ent LAI (annual, DOY or monthly)', 'm^2 m-2', 'Leaf Area Index')
+        fname(nf) = make_fname2(chunker, chunkerlr, esub_p, &
+            .true., 'BNU', 'M', 'lai', 2004, 'pure', 'purelr', '1.1', &
+            'month', imonth)
     end do
 
 
@@ -227,11 +278,32 @@ $$$$$$$$$$$$ WORKING HERE.... $$$$$$$$$
         STOP
     end if
 
+
+
+
+    ! ---------------------------------
+    ! Create single-layer EntSet_t
+    call ent1%allocate(1,1)
+    call ent1%add_covertype('n', 'bs_brightratio', 'Bare Soil Brightness Ratio')
+
+    ! Bare soil Bright Ratio
+    call chunkerlr%file_info(oinfo, ent1, &
+        'BNU', 'M', 'bs_brightratio', 2004, 'purelr', '1.1')
+    fnames1(1) = &
+        make_fname(LAI3G_INPUT, &
+            'lc_lai_ent/', 'bs_brightratio', &   ! 
+            oinfo%dir, 'bs_brightratio', & !oinfo%leaf, &
+            .false., &
+            oinfo%vname, oinfo%long_name, oinfo%units)
+
+    call regrid_lais(ent1, fnames1)
+    ! ---------------------------------
+
+    esub_p => esub
     do i0=1,nf,10
         i1 = min(nf,i0 + 10 - 1)
 
-!        print *,i0,i1
-        call regrid_lais(esub, fname(i0:i1))
+        call regrid_lais(esub_p, fname(i0:i1))
     end do
 
 end subroutine do_regrid_all_lais
