6993173a49d7f4881f178b37649f6e87e7723418
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-07-29 15:06:36 -0400

    Fixed run_all.sh, removed debugging stuff

--- bnu/run_all.sh
+++ bnu/run_all.sh
@@ -8,12 +8,11 @@ for stage in \
         A01_lc_laimax.F90 \
         A02_lc_lai_doy.F90 \
         A03_lc_lai_monthly.F90 \
-        A04_trim_laimax_1kmx1km.F90
-
-#        A05_trim_laidoy_1kmx1km.F90 \
-#        A06_trim_lc_laimax_laimonth.F90 \
-#        A07_regrid.F90 \
-#        A08_trim.F90
+        A04_reclass_annual.F90 \
+        A05_reclass_doy.F90 \
+        A06_reclass_monthly.F90 \
+        A07_regrid.F90 \
+        A08_trim.F90
 do
 
     echo "============================== Running $stage"
