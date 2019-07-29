2d089a758628930f41df5606db8d9c0232ddd19e
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-07-29 15:02:16 -0400

    A07: Change debug region to North America, where it is easy to debug LC problem

--- bnu/A07_regrid.F90
+++ bnu/A07_regrid.F90
@@ -174,8 +174,11 @@ subroutine regrid_lais(esub, fname)
 !subroutine regrid_handles()
 
 #ifdef ENTGVSD_DEBUG
-    do jchunk = chunker%nchunk(2)*3/4,chunker%nchunk(2)*3/4+1
-    do ichunk = chunker%nchunk(1)*3/4,chunker%nchunk(1)*3/4+1
+!    do jchunk = chunker%nchunk(2)*3/4,chunker%nchunk(2)*3/4+1
+!    do ichunk = chunker%nchunk(1)*3/4,chunker%nchunk(1)*3/4+1
+    ! Choose a smalle area to debug
+    do jchunk = 11,12
+    do ichunk = 5,7
 #else
     do jchunk = 1,chunker%nchunk(2)
     do ichunk = 1,chunker%nchunk(1)
