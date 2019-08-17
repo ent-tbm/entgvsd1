31ea1cfbec40e063edd0327498bb77cd53ace0ee
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-08-16 20:45:08 -0400

    tmp; it runs, but slowly

--- bnu/A01b_soil_albedo.F90
+++ bnu/A01b_soil_albedo.F90
@@ -44,18 +44,19 @@ program Carrer_soilalbedo_to_GISS
     implicit none
 
     
-!    integer, parameter :: N_BANDS = 6
+!    integer, parameter :: NBANDS_GISS = 6
 
     !integer, parameter :: NSPEC = 3785 !Number of rows in Lean spectrum
     !character*118 :: GISSBANDStxt = !len = 6*19 + 4
     !Below parameters are moved to EntGVSD_util.f
-!     integer, parameter :: N_BANDS = 6 !GISS 6 spectral bands
+!     integer, parameter :: NBANDS_GISS = 6 !GISS 6 spectral bands
 !      character*101, parameter :: GISSBANDStxt =  &
 !          'VIS (330-770), NIR1 (770-860), NIR2 (860-1250), '// &
 !          'NIR3 (1250-1500), NIR4 (1500-2200), NIR5 (2200-4000)'
 
 
     ! BISS Band Defintions
+    integer, parameter :: VIS_GISS = 1
     integer, parameter :: NBANDS_GISS = 6
     character*4, parameter :: sbands_giss(NBANDS_GISS) = &
         (/"VIS ", "NIR1", "NIR2", "NIR3", "NIR4", "NIR5"/)
@@ -75,7 +76,7 @@ program Carrer_soilalbedo_to_GISS
     !GISS ModelE grey albedo fractions
     integer, parameter :: BRIGHT = 1
     integer, parameter :: DARK = 2
-    character*5, parameter :: sbright_dark(2) = (/"bright", "dark "/)
+    character*6, parameter :: sbright_dark(2) = (/"bright", "dark  "/)
 
     ! Convert spectrum in one set of bands to another set of bands while conserving energy
     !From Judith Lean 0 km solar surface irradiance (from 2006 version)
@@ -91,17 +92,24 @@ program Carrer_soilalbedo_to_GISS
         0.051105118 & !8 2200-4000 nm
       /)
 
-    real*4 :: fracSW_MODIS(2) !fraction of 1-VIS and 2-NIR in SW 300-4000 nm
-    real*4 :: fracSW_GISS(N_BANDS) !fraction of SW W m-2 nm-1 300-4000 nm
+      ! Fraction of shortwave (SW) surface irradiance (200-4000 nmm)
+      !  in GISS bands.
+      ! From solar.lean_TOA.0km_toNancyin2006.xlsx.
+      real*4, parameter :: fracSW_Lean_0km_2006(NBANDS_GISS) = (/ &
+          0.56, 0.0893, 0.2102, 0.0394, 0.0786, 0.0225  /)
+      
 
-    integer :: band(N_BANDS)
+    real*4 :: fracSW_GISS(NBANDS_GISS) !fraction of SW W m-2 nm-1 300-4000 nm
+
+    integer :: band(NBANDS_GISS)
 
     integer :: k, m, b, dimlon, dimlat, dim(2), dimband
     integer :: dg
     real*4 :: s
 
+    type(Chunker_t) :: chunker
     ! Input files
-    type(ChunkIO) :: ioall_lc, io_lcice, io_lcwater
+    type(ChunkIO_t) :: ioall_lc, io_lcice, io_lcwater
     type(ChunkIO_t) :: ioall_albmodis(NBANDS_MODIS)
     type(ChunkIO_t) :: io_albmodis(NSTATS,NBANDS_MODIS)
     ! Output Files
@@ -111,7 +119,8 @@ program Carrer_soilalbedo_to_GISS
     type(ChunkIO_t) :: ioall_fracbd(NBANDS_GISS), io_fracbd(BRIGHT_DARK,NBANDS_GISS)
     type(ChunkIO_t) :: ioall_fracgrey, io_fracgrey(BRIGHT_DARK)
 
-        integer :: iband,istat
+    INTEGER :: ichunk,jchunk,ic,jc
+    integer :: iband,istat
     real, dimension(:,:), allocatable, target :: wta    ! Surmised landmask
     type(EntSet_t) :: ent2
     type(FileInfo_t) :: info
@@ -124,7 +133,7 @@ program Carrer_soilalbedo_to_GISS
     !* Select spectral band irradiance fractions
     fracSW_GISS(:) = fracSW_Lean_0km_2006(:)
 
-    call chunker%init(IMK, JMK, IMH*2,JMH*2, 'qxq', 10, 10, 10)!, (/6,5/))
+    call chunker%init(IMK, JMK, IMH*2,JMH*2, 'qxq', 100, 100, 10)!, (/6,5/))
     allocate(wta(chunker%chunk_size(1), chunker%chunk_size(2)))
 
     ! ------------- Open Input Files
@@ -133,11 +142,11 @@ program Carrer_soilalbedo_to_GISS
     call chunker%file_info(info, ent20, 'BNU', 'M', 'lc', 2004, 'ent17', '1.1')
     call chunker%nc_open(ioall_lc, &
         LC_LAI_ENT_DIR, trim(info%dir), trim(info%leaf)//'.nc', trim(info%vname), 0)
-    call chunkerlr%nc_reuse_var(ioall_lc, io_lcice, (/1,1,ent2%svm(SNOW_ICE)/))
-    call chunkerlr%nc_reuse_var(ioall_lc, io_lcwater, (/1,1,ent2%svm(CV_WATER)/))
+    call chunker%nc_reuse_var(ioall_lc, io_lcice, (/1,1,ent2%svm(SNOW_ICE)/))
+    call chunker%nc_reuse_var(ioall_lc, io_lcwater, (/1,1,ent2%svm(CV_WATER)/))
 
     ! ------------ albmodis
-    do iband=1,NBANDS_CARRER
+    do iband=1,NBANDS_MODIS
         call chunker%nc_open(ioall_albmodis(iband), LC_LAI_ENT_DIR, &
             'carrer/', &
             'soilalb_'//trim(sbands_modis(iband))//'.nc', &
@@ -161,28 +170,28 @@ program Carrer_soilalbedo_to_GISS
     do iband=1,NBANDS_GISS
         call chunker%nc_create(io_albgiss(iband), weighting(wta,1d0,0d0), &
             'carrer/', &
-            'albgiss'//trim(sbands_giss(iband)), &
-            'albgiss'//trim(sbands_giss(iband)), &
+            'albgiss_'//trim(sbands_giss(iband)), &
+            'albgiss_'//trim(sbands_giss(iband)), &
             'Soil albedo in GISS bands', '1')
     end do
 
     ! ----------- fracbd
     do iband=1,NBANDS_GISS
-        call chunker%nc_create(io_fracbd(iband), weighting(wta,1d0,0d0), &
+        call chunker%nc_create(ioall_fracbd(iband), weighting(wta,1d0,0d0), &
             'carrer/', &
-            'fracbd'//trim(sbands_giss(iband)), &
-            'fracbd'//trim(sbands_giss(iband)), &
+            'fracbd_'//trim(sbands_giss(iband)), &
+            'fracbd_'//trim(sbands_giss(iband)), &
             'Bright/Dark Soil in GISS Bands', '1', &
             sbright_dark)
-        do k=1,2
+        do k=1,BRIGHT_DARK
             call chunker%nc_reuse_var( &
                 ioall_fracbd(iband), io_fracbd(k,iband), &
-                (/1,1,k/))
+                (/1,1,k/), weighting(wta,1d0,0d0))
         end do
     end do
 
     ! -------------- fracgrey
-    call chunker%nc_create(io_fracgrey, weighting(wta,1d0,0d0), &
+    call chunker%nc_create(ioall_fracgrey, weighting(wta,1d0,0d0), &
         'carrer/', &
         'fracgrey', &
         'fracgrey', &
@@ -191,7 +200,7 @@ program Carrer_soilalbedo_to_GISS
     do k=1,2
         call chunker%nc_reuse_var( &
             ioall_fracgrey, io_fracgrey(k), &
-            (/1,1,k/))
+            (/1,1,k/), weighting(wta,1d0,0d0))
     end do
 
 
@@ -211,24 +220,28 @@ program Carrer_soilalbedo_to_GISS
         wta = 0
 
         do jc = 1,chunker%chunk_size(2)
+print *,jc
         do ic = 1,chunker%chunk_size(1)
 
+
+
+#if 1
             ! Compute overall NetCDF index of current cell
             !ii = (ichunk-1)*chunker%chunk_size(1)+(ic-1)+1
             !jj = (jchunk-1)*chunker%chunk_size(2)+(jc-1)+1
 
             ! Infer soil mask
             ! TODO: Get this from LC instead
-            if (io_albmodis(SMEAN,1)%buf(ic,jc) /= FillValue)
+            if (io_albmodis(SMEAN,1)%buf(ic,jc) /= FillValue) then
                 wta(ic,jc) = 1
             else
-                sta(ic,jc) = 0
+                wta(ic,jc) = 0
             end if
 
 
             !* Calculate total SW albedo --------------------------------------
-            if ((albmodis(SMEAN, VIS_MODIS).eq.FillValue).or. &
-                (albmodis(SMEAN, NIR_MODIS).eq.FillValue)) then
+            if ((io_albmodis(SMEAN, VIS_MODIS)%buf(ic,jc).eq.FillValue).or. &
+                (io_albmodis(SMEAN, NIR_MODIS)%buf(ic,jc).eq.FillValue)) then
                 io_albsw%buf(ic,jc) = FillValue
             else
                 io_albsw%buf(ic,jc) = ( &
@@ -251,15 +264,15 @@ program Carrer_soilalbedo_to_GISS
                 io_albgiss(VIS_GISS)%buf(ic,jc) = &
                    ( io_albmodis(SMEAN,VIS_MODIS)%buf(ic,jc) * &
                    (fracSW_MG(nm300_400) + fracSW_MG(nm400_700)) + &
-                   io_albmodis(SMEAN,NIR_MODS)%buf(ic,jc)*fracSW_MG(nm700_770)) &
+                   io_albmodis(SMEAN,NIR_MODIS)%buf(ic,jc)*fracSW_MG(nm700_770)) &
                    /( fracSW_MG(nm300_400) + fracSW_MG(nm400_700) + &
                    fracSW_MG(nm700_770) )
                 do k=2,NBANDS_GISS
                     io_albgiss(k)%buf(ic,jc) = io_albmodis(SMEAN, NIR_MODIS)%buf(ic,jc)
                 end do
             endif
-
-
+#endif
+#if 0
             !------ Old VEG bare_bright vs. bare_dark soil fractions
             !* Calculate GISS ModelE grey albedo bright vs. dark fractions
             !Same netcdf names as in Ent_pfts.
@@ -267,8 +280,12 @@ program Carrer_soilalbedo_to_GISS
             !  For partial-land cells (that have ice and/or water) use
             ! soil albedo of nearest all-land neighbor.  If none, default to
             ! to cell's mixed albedo.
-            fracbd(:,:,:,:) = FillValue
-            do k=1,N_BANDS
+            do iband=1,NBANDS_GISS
+            do k=1,2
+                io_fracbd(k,iband)%buf = FillValue
+            end do
+            end do
+            do k=1,NBANDS_GISS
                 if (io_albgiss(k)%buf(ic,jc) /= FillValue) then
                     if (abs(1d0 - (io_lcice%buf(ic,jc)+io_lcwater%buf(ic,jc))) < 1d-5) then
                         !All permanent ice, no soil.
@@ -323,13 +340,13 @@ program Carrer_soilalbedo_to_GISS
             !  For partial-land cells (that have water) use
             ! soil albedo of nearest all-land/non-ice neighbor.  If none, default to
             ! to cell's mixed albedo.
-            io_fracgrey(BRIGHT)%buf(:,:) = 0
-            io_fracgrey(DARK)%buf(:,:) = 0
+            io_fracgrey(BRIGHT)%buf(ic,jc) = 0
+            io_fracgrey(DARK)%buf(ic,jc) = 0
 
             if (io_albsw%buf(ic,jc).ne.FillValue) then 
                if ((io_lcice%buf(ic,jc)+io_lcwater%buf(ic,jc)).eq.1.0) then !No ground
-                    io_fracgrey(BRIGHT)%buf(:,:) = FillValue
-                    io_fracgrey(DARK)%buf(:,:) = FillValue
+                    io_fracgrey(BRIGHT)%buf(ic,jc) = FillValue
+                    io_fracgrey(DARK)%buf(ic,jc) = FillValue
                elseif (((io_lcice%buf(ic,jc)+io_lcwater%buf(ic,jc)).gt.0.).and. &
                       ((io_lcice%buf(ic,jc)+io_lcwater%buf(ic,jc)).lt.1.0)) then !Partial
                   !Search for nearest grid cell up to 5 that is all ground
@@ -367,9 +384,10 @@ program Carrer_soilalbedo_to_GISS
                   io_fracgrey(DARK)%buf(ic,jc) = 1d0 - io_fracgrey(BRIGHT)%buf(ic,jc)
                endif
             else
-                io_fracgrey(BRIGHT)%buf(:,:) = FillValue
-                io_fracgrey(DARK)%buf(:,:) = FillValue
+                io_fracgrey(BRIGHT)%buf(ic,jc) = FillValue
+                io_fracgrey(DARK)%buf(ic,jc) = FillValue
             endif
+#endif
 
         end do
         end do
