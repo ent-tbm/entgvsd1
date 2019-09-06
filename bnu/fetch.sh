3c22b0821635c0742e970667b5258f0852c9105c
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-09-06 13:01:45 -0400

    Updated fetch script for latest run

--- bnu/fetch.sh
+++ bnu/fetch.sh
@@ -1,7 +1,8 @@
 # Fetch raw/ and pure/ --- but only _qxq lo-res versions
-#rsync  -av gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent . --filter='+ *_qxq.nc' --filter='+ *_plot.nc' --filter='- *.nc' --filter='- *.tar.gz'
-
-rsync -av gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent/png lc_lai_ent
+rsync  -av gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent . --filter='+ *_qxq.nc' --filter='+ *_plot.nc' --filter='- *.nc' --filter='- *.tar.gz' --filter='- *.pdf'
 
 ## Fetch everything else
-#rsync  -av --filter='- /lc_lai_ent/ent17' --filter='- /lc_lai_ent/carrer' --filter='- /lc_lai_ent/pure' gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent . 
+rsync  -av --filter='- /lc_lai_ent/ent17' --filter='- /lc_lai_ent/carrer' --filter='- /lc_lai_ent/pure' gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent . 
+
+# Fetch plots
+#rsync -av gibbs:/home2/rpfische/git/entgvsd1/bnu/lc_lai_ent/png lc_lai_ent
