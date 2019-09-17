82197169a9fd24bec0a731c3f0ecb9f65e9fd602
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-09-16 22:27:47 -0400

    On-the-fly regridding for checksums seems to work

--- bnu/A09_checksums.F90
+++ bnu/A09_checksums.F90
@@ -5,6 +5,7 @@ use ent_labels_mod
 use geom_mod
 use paths_mod
 use gcm_labels_mod
+use hntr_mod
 
 implicit none
 CONTAINS
@@ -64,6 +65,90 @@ subroutine ent_diff(chunker, &
 
 end subroutine ent_diff
 
+
+
+subroutine ent_diff_lr(chunker, chunker_hr, &
+    info1, &
+    iroot2, idir2, ileaf2, vname2, &
+    create_lr)
+
+    type(Chunker_t) :: chunker, chunker_hr
+    integer :: im,jm
+    type(FileInfo_t) :: info1
+    character*(*), intent(IN) :: iroot2,idir2,ileaf2,vname2
+    logical, intent(IN), OPTIONAL :: create_lr
+    ! -------------- Locals
+    real*4 :: v1,v2
+    real*4, allocatable, dimension(:,:) :: buf2
+
+    type(ChunkIO_t) :: xin1, xin2_hr, xout
+    integer :: ichunk,jchunk, ic,jc
+
+    type(HntrSpec_t) :: spec_hr, spec
+    type(HntrCalc_t) :: hntr_lr    ! Preparation to regrid
+
+    call chunker%nc_open(xin1, &
+        LC_LAI_ENT_DIR, info1%dir, trim(info1%leaf)//'.nc', info1%vname, 1)
+
+    call chunker_hr%nc_open(xin2_hr, &
+        iroot2, idir2, ileaf2, vname2, 1)
+
+    call chunker%nc_create(xout, weighting(chunker%wta1,1d0,0d0), &
+        'checksum/'//trim(info1%dir), trim(info1%leaf)//'_diff', trim(info1%vname)//'_diff', &
+        'checksum', '1', create_lr=create_lr)
+    
+    call chunker%nc_check('A09_checksums')  ! will get overwritten
+    call chunker_hr%nc_check('A09_checksums')  ! will get overwritten
+
+
+    ! Hntr stuff
+    spec = hntr_spec(chunker%chunk_size(1), chunker%ngrid(2), 0d0, 180d0*60d0 / chunker%ngrid(2))
+    spec_hr = hntr_spec(chunker_hr%chunk_size(1), chunker_hr%ngrid(2), 0d0, 180d0*60d0 / chunker_hr%ngrid(2))
+    hntr_lr = hntr_calc(spec, spec_hr, 0d0)   ! datmis=0
+
+
+    allocate(buf2(chunker%chunk_size(1), chunker%chunk_size(2)))
+
+#ifdef ENTGVSD_DEBUG
+    do jchunk = 1,1
+    do ichunk = 1,1
+#else
+    do jchunk = 1,chunker%nchunk(2)
+    do ichunk = 1,chunker%nchunk(1)
+#endif
+        call chunker%move_to(ichunk,jchunk)
+        call chunker_hr%move_to(ichunk,jchunk)
+
+        call hntr_lr%regrid4( &
+            buf2, xin2_hr%buf, &
+            chunker_hr%wta1, 1d0, 0d0, &   ! weighting
+            xout%startB(2), xout%chunker%chunk_size(2))
+
+        do jc = 1,chunker%chunk_size(2)
+        do ic = 1,chunker%chunk_size(1)
+
+            v1 = xin1%buf(ic,jc)
+            v2 = buf2(ic,jc)
+
+            if ((v1/=v1).and.(v2/=v2)) then
+                xout%buf(ic,jc) = 0.   ! They match!
+            else
+                xout%buf(ic,jc) = v1-v2
+            end if
+
+        end do
+        end do
+
+        call chunker%write_chunks
+        
+    end do
+    end do
+
+end subroutine ent_diff_lr
+
+
+
+
 subroutine init_hr(chunker)
     type(Chunker_t) :: chunker
     call chunker%init(im1km,jm1km,imh,jmh,'forplot',2,1,1, nchunk=(/6,5/))
@@ -77,7 +162,7 @@ subroutine do_A09_checksums(esub_p)
     type(Chunker_t) :: chunker
     integer :: idoy, imonth
 
-#if 0
+#if 1
     ! ----------- A01
     print *,'========================= A01'
     call init_hr(chunker)
@@ -136,7 +221,7 @@ subroutine do_A09_checksums(esub_p)
 #endif
 
 ! Un-comment after we rerun
-#if 0
+#if 1
     call init_hr(chunker)
     call chunker%file_info(info, esub_p, &
         'BNU', 'M', 'lclaimax', 2004, 'pure', '1.1', &
@@ -177,9 +262,11 @@ subroutine do_A09_checksums(esub_p)
     end do
 end subroutine
 
-subroutine init_lr(chunker)
+subroutine init_lr(chunker,chunker_hr)
     type(Chunker_t) :: chunker
-    call chunker%init(IMLR,JMLR,  IMLR,JMLR, 'forplot', 2, 1, 1, (/1,1/))
+    type(Chunker_t) :: chunker_hr
+    call chunker%init(IMLR,JMLR,  IMLR,JMLR, 'forplot', 2, 1, 1, (/6,5/))
+    call chunker_hr%init(im1km,jm1km,imh,jmh,'forplot',2,1,1, nchunk=(/6,5/))
 end subroutine
 
 subroutine do_A08_checksums(esub_p, step)
@@ -188,33 +275,34 @@ subroutine do_A08_checksums(esub_p, step)
     ! --------- Locals
     type(FileInfo_t) :: info
     type(Chunker_t) :: chunker
+    type(Chunker_t) :: chunker_hr
     integer :: idoy, imonth
 
 
     ! --------------- A08_trim
     print *,'========================= A08 ',trim(step)
-    call init_lr(chunker)
+    call init_lr(chunker, chunker_hr)
     call chunker%file_info(info, esub_p, 'BNU', 'M', 'lclaimax', 2004, step, '1.1', &
         varsuffix = '_checksum')
     if (LAI_SOURCE == 'LAI3g') then
-        call ent_diff(chunker, info, &
+        call ent_diff_lr(chunker, chunker_hr, info, &
             DATA_INPUT, 'LAI/', 'LAI3gMax_1kmx1km.nc', 'laimax', create_lr=.false.)
     else if (LAI_SOURCE == 'BNU') then
-        call ent_diff(chunker, info, &
+        call ent_diff_lr(chunker, chunker_hr, info, &
             LC_LAI_ENT_DIR, 'bnu/', 'bnu_laimax.nc', 'laimax', create_lr=.false.)
     end if
 
-    call init_lr(chunker)
+    call init_lr(chunker, chunker_hr)
     call chunker%file_info(info, esub_p, 'BNU', 'M', 'lchgt', 2004, step, '1.1', &
         varsuffix = '_checksum')
-    call ent_diff(chunker, info, &
+    call ent_diff_lr(chunker, chunker_hr, info, &
         DATA_INPUT, 'height/', 'simard_forest_heights.nc', 'heights', create_lr=.false.)
 
     do imonth=1,NMONTH
-        call init_lr(chunker)
+        call init_lr(chunker, chunker_hr)
         call chunker%file_info(info, esub_p, 'BNU', 'M', 'lclai', 2004, step, '1.1', &
             doytype='month', idoy=imonth, varsuffix='_checksum')
-        call ent_diff(chunker, info, &
+        call ent_diff_lr(chunker, chunker_hr, info, &
             DATA_INPUT, 'LAI/BNUMonthly/', 'global_30s_2004_'//MONTH(imonth)//'.nc', &
             'lai', create_lr=.false.)
     end do
