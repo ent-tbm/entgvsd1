d156e216dc6690ea546dafd72f3f48389660f5fc
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-11-19 11:16:45 -0500

    remove machine-generated .mk files.  The really need to be machine generated.

--- src/mk/A00b_regrid_lr.mk
+++
@@ -1,7 +0,0 @@
-A00b_regrid_lr_INPUTS = \
-
-A00b_regrid_lr_OUTPUTS = \
-    lc_lai_ent/ent17/V6km_EntGVSD17M_BNUM_lc_2004_ann_ent17_v1.1.nc \
-
-
-
--- src/mk/A01a_carrer_mean.mk
+++
@@ -1,8 +0,0 @@
-A01a_carrer_mean_INPUTS = \
-    /home2/rpfische/git/entgvsd1/inputs_manual/carrer/carrer.nc \
-
-A01a_carrer_mean_OUTPUTS = \
-    lc_lai_ent/carrer/albmodis_NIR.nc \
-
-
-
